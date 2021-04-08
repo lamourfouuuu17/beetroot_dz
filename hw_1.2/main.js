29b298e2e49f94d14ea555a7870fcd1f22aa3738
lamourfouuuu17 <samsonenko_anna_ka-96@gmail.com>
2021-04-08 10:42:30 +0300

    1.2 hw js

---
+++ hw_1.2/main.js
@@ -0,0 +1,23 @@
+let num = prompt("Введіть число від 0 до 9");
+
+if (num == 0) {
+  alert("=");
+} else if (num == 1) {
+  alert("!");
+} else if (num == 2) {
+  alert("@");
+} else if (num == 3) {
+  alert("#");
+} else if (num == 4) {
+  alert("$");
+} else if (num == 5) {
+  alert("%");
+} else if (num == 6) {
+  alert("^");
+} else if (num == 7) {
+  alert("&");
+} else if (num == 8) {
+  alert("*");
+} else if (num == 9) {
+  alert("(");
+}
